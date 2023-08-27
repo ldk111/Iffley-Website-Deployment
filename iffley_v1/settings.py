736bc8b8f09abce1d78d9f231d871fbf6a50147d
ldk111 <luke.david.kennedy@gmail.com>
2023-08-27 22:06:12 +0100

    change to using environment variables

--- iffley_v1/settings.py
+++ iffley_v1/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 
 from pathlib import Path
 from decouple import config
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -21,10 +22,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = config("SECRET_KEY")
+SECRET_KEY = os.environ.get("DJANGO_SECRET_KEY", 'django-insecure-d#i%=%i5b%y&@pn6s)_i@2n_-9p8$@a$$msz)e1uqq1__9w^n0')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = os.environ.get('DJANGO_DEBUG', '') != 'False'
 
 ALLOWED_HOSTS = []
 
