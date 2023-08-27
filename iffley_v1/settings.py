dd6582913e74e2c81d84a1e144a295cd687ea696
ldk111 <luke.david.kennedy@gmail.com>
2023-08-27 22:46:06 +0100

    update allowed hosts

--- iffley_v1/settings.py
+++ iffley_v1/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = os.environ.get("DJANGO_SECRET_KEY", 'django-insecure-d#i%=%i5b%y&@p
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.environ.get('DJANGO_DEBUG', '') != 'False'
 
-ALLOWED_HOSTS = ['https://web-production-c0ad.up.railway.app/', '127.0.0.1']
+ALLOWED_HOSTS = ['.railway.com', '127.0.0.1']
 
 CSRF_TRUSTED_ORIGINS = ['https://*.railway.app']
 # Application definition
