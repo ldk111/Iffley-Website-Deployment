2b282fe48a3e9e777e7b9ce82254e8753bedc38d
ldk111 <luke.david.kennedy@gmail.com>
2023-10-02 17:00:31 +0100

    added circuits, sticky navbar, added footer, words

--- iffley_app/views.py
+++ iffley_app/views.py
@@ -67,7 +67,20 @@ def circuits(request):
 
     routes = Route.objects.all()
 
-    circuits = {"The Freshers Circuit": ["Easy Ladder", "Traverse", "Twisted Ladder", "Severely Unimpressed", "Girdering for Beginners", "Sideways", "Rock Over", "Press", "Dobsons Reach", "Sit Start for Beginners", "Five Finger Discount", "Lock, Slot, and Two Smoking Barrels"]}
+    circuits = {"The Freshers Circuit": ["Easy Ladder", "Traverse", "Twisted Ladder", "Severely Unimpressed", "Girdering for Beginners", "Sideways", "Rock Over", "Press", "Dobsons Reach", "Sit Start for Beginners", "Five Finger Discount", "Lock, Slot, and Two Smoking Barrels"],
+                "The Welcome Circuit": ["The Ladder", "The Rocker", "Buzz, Buzz goes the Honey Bee", "The Right Stuff", "Ali G"],
+                "The Purple Circuit": ["Mean Feet", "The Rocker", "Eliminot", "Baby Nemo", "The Slide", "Bobbing Apples", "Baby Starfish", "Ali G", "The Right Stuff", "Praying Mantis", "Jack-In-The-Box", "The Sting"],
+                "The Green Circuit": ["Dynosaur", "Capital One", "The Chinese Leg Extender", "Question Time", "Pebble Beach", "The Good", "Shortcake", "Starry Sky", "Bonfire Night", "Buzz goes the Honey Bee", "Magic Journey", "Ice Cream Man"],
+                "The Kids White Circuit": ["The Ladder", "The Jester", "Foot Up!", "Cat's Cradle", "Cosmic", "The Ugly", "Ice Cube", "Ali G", "Mittens", "Jug-O-Rama", "'Boing!' Said Zebedee", "Wax On, Wax Off"],
+                "The Teal Circuit": ["Varsity", "Main Street", "Underwear Everywhere", "Corkscrew", "Ketch", "Dynamite", "Go Go Gadget", "Touch Me Direct", "Benightment Banjo"],
+                "The Yellow Circuit": ["More Rough Than Diamond", "The Gardener's Legs", "Varsity", "Young Pretender", "And Think of England", "Star Spangled Banner", "Tiffin", "Ice Maker", "Deja Vu", "Angry Beaver", "Weak Like Small Bull", "Pipe Dream"],
+                "The Pink Circuit": ["Klingon", "Black Cuillin", "Hug The Wall", "Minimal Impact", "Problem 1", "Spiky Big Angel", "Major League", "Squetch", "Pull My Finger", "Threesome", "Blair Witch for Beginners", "The Apes of Wrath", "Strong Like a Left Handed Bull", "My Name is Neo"],
+                "The Blue Circuit": ["Stroll", "Slip Knot", "Hebrews 10.9", "Die Gerbils!", "Happy Fun Ball", "Overlap", "The Matrix", "Dinorwic Rainbow", "Masters of Stone", "The Frog", "Moby Dick", "Smart Like Tractor", "California Girl", "Stone Monkey", "The Sword"],
+                "The Sky Blue Circuit": ["Get on the Good Foot", "King Heroin", "Tom's Extension", "Povey For Beginners", "Startime", "Foundations of Funk", "Too Funky in Here"],
+                "The Red Circuit": ["PARC Analysis", "The Bad Touch", "Hebrews 5.10", "Resurrection", "Analogue", "The Tensor", "Troubled Times", "Superman in Y-Fronts", "Jenga", "Twisted Sister", "Death Pirate", "Digital", "Land of Hope and Glory", "Wet Paint", "Black Monday", "Korfball", "Hate Mail"],
+                "The Black Circuit": ["November Rain", "The Very Small Direct", "Tom's Dyno", "Dyno 2", "Dyno 8", "Dyno 3", "Hail Ye, Bob!", "Dyno 10", "The Sword Variation", "The Sabre Dance", "Dyno 11"],
+                "The Crushed Strawberry Circuit": ["American Vision", "Kiss the Wall", "SuperB", "Beshelved", "Crushed Strawberry", "No Angel", "Voyager", "Brown's Eliminate", "Goldfinger", "Technical Apprentice", "Sven the Time Comes", "Geronimo!", "Laaa!", "Icarus"],
+                "The White Circuit": ["Ambiguity", "Tall Man Rides a Shovelhead", "Twister Variation", "Horny Little Devil", "Ortho", "Palm Beach", "The Blair Witch Project", "Event Horizon", "Crack Suicide Squad", "This is a Low", "Nantucket Sleigh Ride", "Zebedee's Torment", "Electric Penguin"]}
                  
     context = {'routes': routes, "circuits": circuits}
 
